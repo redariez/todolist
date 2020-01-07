fe347dbf41d428bf53784c4eb36ce6fcd8ed7679
ade bakari <redariez@gmail.com>
2020-01-06 21:48:35 -0800

    JS added

---
+++ resources/js/main.js
@@ -0,0 +1,12 @@
+
+//User Clicked on the add button
+// If there is any text insisde the item field, add that text to do list
+document.getElementById('add').addEventListener('click', function() {
+var value = document.getElementById('item').value;
+if (value) {
+    
+ }
+}); 
+
+
+
